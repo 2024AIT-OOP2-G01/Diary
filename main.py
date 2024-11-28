c936709a7f72308d6f28df8ac60b49d506d0e216
Miyagoshi_Sota <k23122kk@aitech.ac.jp>
2024-11-28 15:54:41 +0900

    fix:show all diary

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ from diaries.MiyagoshiDiary import MiyagoshiDiary
 from diaries.KunouDiary import KunouDiary
 from diaries.YokomoriDiary import YokomoriDiary
 from diaries.AkieDiary import AkieDiary
+from diaries.okuyamaDiary import okuyamaDiary
+from diaries.WatanabeDiary import WatanabeDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [MiyagoshiDiary(), KunouDiary(), YokomoriDiary(), AkieDiary()]
+diaries = [
+    MiyagoshiDiary(),
+    KunouDiary(),
+    YokomoriDiary(),
+    AkieDiary(),
+    okuyamaDiary(),
+    WatanabeDiary()
+]
 
 for d in diaries:
     print("---------------------------------")
